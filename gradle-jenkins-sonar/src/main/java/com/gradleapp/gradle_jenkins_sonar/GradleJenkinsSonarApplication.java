fa32c4333d143da75127dd4f1825a9ecb76fc799
CX <cx@CXs-MacBook-Pro.attlocal.net>
2024-10-18 20:28:20 -0700

    init

---
+++ gradle-jenkins-sonar/src/main/java/com/gradleapp/gradle_jenkins_sonar/GradleJenkinsSonarApplication.java
@@ -0,0 +1,13 @@
+package com.gradleapp.gradle_jenkins_sonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleJenkinsSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleJenkinsSonarApplication.class, args);
+	}
+
+}
---
+++ gradle-jenkins-sonar/src/test/java/com/gradleapp/gradle_jenkins_sonar/GradleJenkinsSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradleapp.gradle_jenkins_sonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleJenkinsSonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
