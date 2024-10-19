1a25ee68a31c39cd6e6707fb5b3338ef293c28a4
CX <cx@CXs-MacBook-Pro.attlocal.net>
2024-10-18 21:07:59 -0700

    one file

---
+++ gradle-jenkins-sonar/src/main/java/com/gradleapp/gradle_jenkins_sonar/hello/helloWorld.java
@@ -0,0 +1,15 @@
+package com.gradleapp.gradle_jenkins_sonar.hello;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class helloWorld {
+    @GetMapping("/hi")
+    public String index() {
+        return "hello";
+    }
+}
